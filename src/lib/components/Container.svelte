20b40db2af8d928659faaab1104b984f75052b48
Hyziri <hyziri@protonmail.com>
2026-07-21 05:23:50 -0500

    feat: shared container component
    Implement a shared container component to reduce repetition. Use grids instead of flex-wrap for more predictable layout behavior. Remove some redundant div classes.
    
    Fix:
    - Images not rendering due to removal of svelte enhanced images

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let { children, class: className = '' } = $props();
+</script>
+
+<div
+    class="container mx-auto px-4 flex flex-col {className}"
+>
+    {@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 	import Faq from "$lib/components/Faq.svelte";
 	import MemberChart from "$lib/components/MemberChart.svelte";
 	import Page from "$lib/components/page.svelte"
+	import Container from "$lib/components/Container.svelte";
 
 	function scrollToEndgame() {
 		document.getElementById('endgame')?.scrollIntoView({ behavior: 'smooth' });
@@ -24,14 +25,13 @@
 	{@render heroSection()}
 	{@render endgameSection()}
 	{@render learningCurveSection()}
-	{@render whatSetsApartSection()}
 	{@render beginJourneySection()}
 	{@render faqSection()}
 </Page>
 
 {#snippet heroSection()}
 	<section class="flex justify-center h-screen pt-88px pb-22px">
-		<div class="container mx-auto w-full h-full flex flex-col items-center">
+		<Container class="items-center">
 			<div class="flex flex-col w-full my-auto gap-4 lg:mb-66px items-center text-center md:text-left md:items-start md:w-3/4 text-white">
 				<div class="flex flex-col items-center md:items-start gap-2">
 					<div class="flex items-end gap-2">
@@ -65,54 +65,52 @@
 				<p class="text-2xl font-bold">Learn More</p>
 				<Fa icon={faChevronDown} size="lg"/>
 			</div>
-		</div>
+		</Container>
 	</section>
 {/snippet}
 
 {#snippet endgameSection()}
 	<section id="endgame" class="flex justify-center bg-base-100">
-		<div class="container mx-auto h-full flex items-center flex-col gap-8 pb-12">
-			<div class="flex flex-col">
-				<div class="flex justify-center">
-					<h1 class="text-center text-gradient font-bold py-12 text-2xl xl:text-4xl">What Does Your Endgame Look Like?</h1>
-				</div>
-				<ul class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-4 2xl:w-1440px px-4">
+		<Container class="items-center gap-8 py-12">
+		    <h1 class="text-center font-bold text-2xl xl:text-4xl">What Does Your Endgame Look Like?</h1>
+			<div class="flex flex-col gap-8 items-center">
+				<ul class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-4 2xl:w-1440px">
 					{#each ENDGAMES as endgame}
-						<li class="flex flex-col gap-2 bg-base-300 rounded-box items-center text-center px-2 py-6">
+						<li class="flex flex-col gap-2 bg-base-300 rounded-box items-center text-center py-6">
 							<img src={endgame.imageSrc} class="w-300px h-200px" alt={endgame.imageAlt}/>
 							<h2 class="border-b-2 pb-1 border-neutral-700 w-fit text-lg font-bold">{endgame.title}</h2>
 							<p>{endgame.description}</p>
 						</li>
 					{/each}
 				</ul>
-			</div>
-			<div class="w-full sm:w-3/4">
-				<div class="relative w-full aspect-video bg-base-300 rounded-box overflow-hidden">
-					<div class="absolute inset-0 flex items-center justify-center text-gray-400">
-						<p>Video unavailable</p>
-					</div>
-					<iframe class="absolute w-full h-full top-0 left-0" src={FEATURED_VIDEO} title="YouTube video player" frameborder="0" allowfullscreen></iframe>
-				</div>
-			</div>
-			<h3 class="text-xl text-center font-bold">No matter what your endgame is, Autumn's goal is to get you there.</h3>
-		</div>
+    			<div class="w-full sm:w-3/4">
+    				<div class="relative w-full aspect-video bg-base-300 rounded-box overflow-hidden">
+    					<div class="absolute inset-0 flex items-center justify-center text-gray-400">
+    						<p>Video unavailable</p>
+    					</div>
+    					<iframe class="absolute w-full h-full top-0 left-0" src={FEATURED_VIDEO} title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+    				</div>
+    			</div>
+    			<h3 class="text-xl text-center font-bold">No matter what your endgame is, Autumn's goal is to get you there.</h3>
+            </div>
+		</Container>
 	</section>
 {/snippet}
 
 {#snippet learningCurveSection()}
 	<section class="flex flex-col items-center bg-base-100">
-		<div class="container mx-auto flex flex-col items-center">
+		<Container class="items-center gap-8 py-12">
 			<div class="flex flex-col text-center">
-				<h1 class="text-gradient font-bold py-4 text-xl sm:text-2xl lg:text-3xl text-center">Conquer the EVE Learning Curve</h1>
+				<h1 class="font-bold text-xl sm:text-2xl lg:text-3xl text-center">Conquer the EVE Learning Curve</h1>
 				<h2 class="text-sm sm:text-base xl:text-lg text-center">EVE's learning curve is steep but worth it, have support &amp; community every step of the way towards realizing your dreams in EVE Online.</h2>
 			</div>
-			<div class="flex flex-wrap py-12">
-    			<div class="pb-10 w-full lg:pb-0 lg:w-1/2 px-0 sm:px-6">
+			<div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+    			<div class="w-full">
     				<div class="rounded-box bg-base-300 overflow-hidden">
     					<img src="/images/landing/the-great-curve.jpg" alt="The steep learning curve of EVE"/>
     				</div>
     			</div>
-    			<ul class="w-full lg:w-1/2 flex flex-col justify-between gap-4 px-0 sm:px-6 lg:px-0">
+    			<ul class="w-full flex flex-col justify-between gap-4">
     				{#each LEARNING_CURVE_ITEMS as { icon: Icon, title, description }}
     					<li class="grid rounded-box bg-base-300 p-4 gap-2">
     						<div class="flex gap-2 items-center">
@@ -124,114 +122,84 @@
     				{/each}
     			</ul>
 			</div>
-		</div>
-	</section>
-{/snippet}
-
-{#snippet whatSetsApartSection()}
-	<section class="flex justify-center items-center bg-base-100">
-		<div class="container mx-auto flex flex-col py-12">
-			<div class="flex justify-center">
-				<h1 class="text-gradient font-bold py-4 text-xl sm:text-2xl lg:text-3xl">What Sets Autumn Apart?</h1>
-			</div>
-			<ul class="flex flex-wrap text-center">
-				<li class="flex flex-col gap-2 items-center w-full lg:w-1/2 p-6">
-					<Fa icon={faAtom} size="lg"/>
-					<h2 class="text-lg font-bold">IT Infrastructure From the Ground Up</h2>
-					<p>In a sea of many alliances &amp; corporations few have the ability to design their own IT infrastructure from the ground up. A distinct advantage when it comes to achieving our goal of being highly organized and highly accessible to even the newest of players.</p>
-				</li>
-				<li class="flex flex-col gap-2 items-center w-full lg:w-1/2 p-6">
-					<Fa icon={faChartLine} size="lg"/>
-					<h2 class="text-lg font-bold">Not Just Education But Also Continued Growth</h2>
-					<p>Not only do we aim to help you start those first steps in EVE Online, we aim to take you all the way towards your end goal. At Autumn investment in our member's growth comes first and foremost.</p>
-				</li>
-			</ul>
-		</div>
+		</Container>
 	</section>
 {/snippet}
 
 {#snippet beginJourneySection()}
 	<section class="flex flex-col items-center bg-base-100">
-		<div class="container mx-auto flex flex-col items-center">
-			<div class="flex flex-col text-center">
-				<h1 class="text-gradient font-bold py-4 text-xl sm:text-2xl lg:text-3xl text-center">Begin Your Journey as Early as Right Now</h1>
+		<Container class="items-center gap-8 py-12">
+		    <h1 class="font-bold text-xl sm:text-2xl lg:text-3xl text-center">Begin Your Journey as Early as Right Now</h1>
+    		<div class="grid grid-cols-1 xl:grid-cols-2 gap-4 xl:gap-8">
+    			<div class="flex flex-col gap-8 items-center">
+                    <h2 class="text-lg sm:text-xl font-bold text-center">Join Many Others in Taking that First Step</h2>
+    				<div class="bg-base-200 rounded-sm shadow-md w-full">
+    					<MemberChart/>
+    				</div>
+    			</div>
+    			<div class="flex flex-col gap-8 items-center">
+                    <h2 class="text-lg sm:text-xl font-bold text-center">Get into the Heart of EVE in Nullsec or Start Small in Highsec</h2>
+    				<ul class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    					{#each CORPORATIONS as corporation}
+    						<li>
+    							<CorporationCard corporation={corporation}/>
+    						</li>
+    					{/each}
+    				</ul>
+    			</div>
+    		</div>
+    		<div class="pt-12">
+    			<div class="grid grid-cols-1 lg:grid-cols-[1fr_auto_1fr] gap-4 lg:gap-8 items-start">
+                    <div class="flex flex-col lg:pr-8">
+       					<h3 class="text-gradient text-2xl font-bold text-center">Chat with a Recruiter</h3>
+       					<div class="flex flex-col items-center gap-2 pt-4">
+      						<h4 class="text-lg font-bold">Reach us Through Either</h4>
+      						<ul class="flex flex-col gap-2 items-center">
+     							<li class="mb-2">
+    								<a href={DISCORD_LINK}>
+       									<button class="btn btn-outline">
+      										<Fa icon={faDiscord} size="lg"/>
+      										<p>The Autumn Discord</p>
+       									</button>
+    								</a>
+     							</li>
+     							<li>The <span class="font-bold">Autumn Public</span> in-game chat channel</li>
+     							<li>EVE Mail or Discord message any of our leadership below</li>
+      						</ul>
+       					</div>
+                    </div>
+         			<div class="hidden lg:block w-px bg-base-content self-stretch bo\"></div>
+         			<div class="flex flex-col lg:pl-8">
+        				<h3 class="text-2xl font-bold text-center">Begin the Application Process</h3>
+        				<ol class="list-decimal max-w-500px mx-auto pt-4 px-8">
+           					<li>Submit your application at <a class="link link-hover" href={APPLY_LINK}>https://join.autumn-order.com/apply</a></li>
+           					<li>Wait for a recruiter to review your application, come chat with us in Discord or Autumn Public while you wait!</li>
+           					<li>Accept your invitation, most applications are reviewed in less than 24 hours.</li>
+           					<li>Follow our Getting Started Guide to get setup and come take part in our community!</li>
+        				</ol>
+         			</div>
+                </div>
+     			<ul class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 pt-12 justify-items-center">
+    				{#each RECRUITERS as recruiter}
+                        <li class="w-full">
+      						<RecruiterCard recruiter={recruiter}/>
+       					</li>
+    				{/each}
+     			</ul>
 			</div>
-		<div class="flex pt-12 flex-wrap">
-			<div class="w-full xl:w-1/2 pb-6 flex flex-col items-center">
-				<div class="w-full flex justify-center pb-4">
-					<h2 class="text-gradient text-lg sm:text-xl font-bold text-center">Join Many Others in Taking that First Step</h2>
-				</div>
-				<div class="bg-base-200 rounded-sm shadow-md w-full h-250px sm:h-350px lg:h-400px max-w-695px">
-					<MemberChart/>
-				</div>
-			</div>
-			<div class="w-full xl:w-1/2 pb-6">
-				<div class="w-full flex justify-center pb-4">
-					<h2 class="text-gradient text-lg sm:text-xl font-bold text-center">Get into the Heart of EVE in Nullsec or Start Small in Highsec</h2>
-				</div>
-				<ul class="flex justify-center gap-4 flex-wrap">
-					{#each CORPORATIONS as corporation}
-						<li>
-							<CorporationCard corporation={corporation}/>
-						</li>
-					{/each}
-				</ul>
-			</div>
-		</div>
-		<div class="flex flex-col pb-12 flex-wrap pt-12">
-			<div class="w-full flex justify-evenly flex-wrap">
-				<div class="flex flex-col flex-1 w-full lg:w-1/3">
-					<h3 class="text-gradient w-full text-2xl font-bold text-center">Chat with a Recruiter</h3>
-					<div class="flex flex-col items-center gap-2  pt-4">
-						<h4 class="text-lg font-bold">Reach us Through Either</h4>
-						<ul class="flex flex-col gap-2">
-							<li class="self-center mb-2">
-								<a href={DISCORD_LINK}>
-									<button class="btn btn-outline">
-										<Fa icon={faDiscord} size="lg"/>
-										<p>The Autumn Discord</p>
-									</button>
-								</a>
-							</li>
-							<li>The <span class="font-bold">Autumn Public</span> in-game chat channel</li>
-							<li>EVE Mail or Discord message any of our leadership below</li>
-						</ul>
-					</div>
-				</div>
-				<div class="">
-					<p class="text-gradient text-2xl font-bold text-center pt-8 lg:pt-0">OR</p>
-				</div>
-				<div class="flex flex-col flex-1 w-full lg:w-1/3">
-					<h3 class="text-gradient w-full text-2xl font-bold text-center pt-8 lg:pt-0">Begin the Application Process</h3>
-					<ol class="list-decimal max-w-500px mx-auto pt-4 px-2">
-						<li>Submit your application at <a class="link link-hover" href={APPLY_LINK}>https://join.autumn-order.com/apply</a></li>
-						<li>Wait for a recruiter to review your application, come chat with us in Discord or Autumn Public while you wait!</li>
-						<li>Accept your invitation, most applications are reviewed in less than 24 hours.</li>
-						<li>Follow our Getting Started Guide to get setup and come take part in our community!</li>
-					</ol>
-				</div>
-			</div>
-			<ul class="w-full flex justify-center pt-12">
-				{#each RECRUITERS as recruiter}
-					<li class="w-full md:w-1/2 xl:w-1/4 max-w-350px">
-						<RecruiterCard recruiter={recruiter}/>
-					</li>
-				{/each}
-			</ul>
-			</div>
-		</div>
+		</Container>
 	</section>
 {/snippet}
 
 {#snippet faqSection()}
 	<section class="flex flex-col items-center py-6 min-h-700px pb-24 bg-base-100">
-		<div class="container mx-auto flex flex-col items-center">
+		<Container>
 			<div>
 				<h1 class="text-gradient font-bold py-4 text-xl sm:text-2xl lg:text-3xl text-center">Frequently Asked Questions</h1>
 			</div>
 			<div class="w-full md:max-w-70%">
 				<Faq questions={FAQ_QUESTIONS}/>
 			</div>
-		</div>
+		</Container>
 	</section>
 {/snippet}
