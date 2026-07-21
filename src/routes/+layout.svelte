32142b661887b43d4dbf9f237088df13dbe33c02
Hyziri <hyziri@protonmail.com>
2026-07-20 23:54:28 -0500

    refactor: remove mode watcher for now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,10 @@
 	import '../app.css';
 	let { children } = $props();
 
-	import { ModeWatcher } from "mode-watcher";
-
 	import Header from "$lib/components/Header.svelte";
 	import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <Header/>
-<ModeWatcher/>
 {@render children()}
 <Footer/>
